8111feff581a24926dc3fbdd720d46dab6699126
MichaelC27 <dbpanama2796@gmail.com>
2022-12-28 11:44:48 -0500

    ejercicio

---
+++ Curso/practica.js
@@ -0,0 +1,9 @@
+const movil = {
+    altura: 10,
+    anchura: 5,
+    marca: "Xiaomi",
+    isWhite: false,
+    contactos: ["Gorka", "Martín", "Raúl"],
+  }
+
+  console.log(movil.contactos)
---
+++ Ejercicios/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,9 @@
+const Usuario = ["MIchael Cordones", 26, true, new Date( "June 27 1996"),  libro={
+  titulo:"El ataud de uso" ,
+  autor:"Rosa María Britton",
+  fecha:"1983",
+  url:"https://www.lecturalia.com/libro/63521/el-ataud-de-uso"
+}]
+
+
+console.log(Usuario);
