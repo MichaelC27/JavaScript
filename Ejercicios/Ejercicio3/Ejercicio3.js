bb83dab5dd11bd9c468130a219b3c6c6dc3940bf
MichaelC27 <dbpanama2796@gmail.com>
2022-12-31 11:43:22 -0500

    ejercicio3

---
+++ Ejercicios/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,34 @@
+let nombre =" Michael";
+let apellido= " Cordones";
+let estudiante= `${nombre} ${apellido}`;
+let estudianteMayus = estudiante.toUpperCase();
+let estudianteMinus = estudiante.toLowerCase();
+let longitud_estudiante = estudiante.length;
+let primera_letra = nombre.charAt(0);
+let ultima_letra = apellido.charAt(7);
+let sin_espacios = estudiante.trim();
+let verifcar = estudiante.includes("Michael");
+
+
+
+
+
+console.log(nombre);
+
+console.log(apellido);
+
+console.log(estudiante);
+
+console.log(estudianteMayus);
+
+console.log(estudianteMinus);
+
+console.log(longitud_estudiante);
+
+console.log(primera_letra);
+
+console.log(ultima_letra);
+
+console.log(sin_espacios);
+
+console.log(verifcar);
