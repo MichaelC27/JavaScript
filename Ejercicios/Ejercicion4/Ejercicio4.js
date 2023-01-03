16000e5b26993ed6d6e7efd50331d17b0f936668
MichaelC27 <dbpanama2796@gmail.com>
2023-01-03 10:45:03 -0500

    ejercicio5

---
+++ Ejercicios/Ejercicion4/Ejercicio4.js
@@ -0,0 +1,12 @@
+const altura_m = 1.65;
+const altura_cm = 165;
+const peso_k = 85.5;
+
+const altura_r = Math.ceil(altura_m)
+console.log(altura_r)
+
+const peso_r = Math.floor(peso_k)
+console.log(peso_r)
+
+const Iguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE
+console.log(Iguales)
