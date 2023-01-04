7f1f5296ca782ea03e8ab6f92827629642aeaa63
MichaelC27 <dbpanama2796@gmail.com>
2023-01-04 11:35:20 -0500

    Ejercicion6_P1

---
+++ Ejercicios/Ejercicio6/conjuntos.js
@@ -0,0 +1,13 @@
+const familia=["michael","ricardo","alan","albe"]
+
+   
+console.log(familia)
+
+const newfamilia = new Set(familia);
+console.log(newfamilia)
+
+newfamilia.add("michael");
+console.log(newfamilia)
+
+newfamilia.add("Javascript")
+console.log(newfamilia)
